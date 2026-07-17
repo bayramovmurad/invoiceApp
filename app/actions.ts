5a75541d7bc6a5ff90eaca186aa80cd7f172fcfd
bayramovmurad <demonseytandir@gmail.com>
2026-07-17 22:47:38 +0300

    feat: created factures function

--- app/actions.ts
+++ app/actions.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import prisma from "@/lib/prisma";
+import { randomBytes } from "crypto";
 
 
 export async function checkAndAddUser(email: string, name: string) {
@@ -23,5 +24,58 @@ export async function checkAndAddUser(email: string, name: string) {
 
     } catch (error) {
         console.error(error)
+    }
+}
+
+const generateUniqueId = async () => {
+    let uniqueId;
+    let isUnique = false;
+
+    while(!isUnique){
+        uniqueId = randomBytes(3).toString('hex')
+        const existingInvoice = await prisma.invoice.findUnique({
+            where:{
+                id:uniqueId
+            }
+        })
+        if(!existingInvoice){
+            isUnique = true;
+        }
+    }
+
+    return uniqueId;
+
+};
+
+export async function createEmptyInvoice(email: string, name: string){
+    try {
+        const user = await prisma.user.findUnique({
+            where:{
+                email: email
+            }
+        });
+
+        const invoiceId = await generateUniqueId() as string;
+
+        if(user){
+            const newInvoice = await prisma.invoice.create({
+                data:{
+                    id: invoiceId,
+                    name: name,
+                    userId: user?.id, 
+                    issuerName: "",
+                    issuerAddress: "",
+                    clientName: "",
+                    clientAddress: "",
+                    invoiceDate: "",
+                    dueDate: "",
+                    vatActive: false,
+                    vatRate: 20,
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        
     }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,88 @@
+"use client";
 import Wrapper from "./components/Wrapper";
+import { Layers } from "lucide-react";
+import { useEffect, useState } from "react";
+import { createEmptyInvoice } from "./actions";
+import { useUser } from "@clerk/nextjs";
 
-export default function page() {
+
+export default function Home() {
+  const { user } = useUser();
+  const [invoiceName, setInvoiceName] = useState("");
+  const [isNameValid, setIsNameValid] = useState(true);
+  const email = user?.primaryEmailAddress?.emailAddress as string;
+
+
+  useEffect(() => {
+    setIsNameValid(invoiceName.length <= 60);
+  }, [invoiceName]);
+
+  const handleCreateInvoice = async () => {
+    try {
+      if (email) {
+        await createEmptyInvoice(email, invoiceName);
+      }
+      setInvoiceName("");
+      const modal = document.getElementById("my_modal_3") as HTMLDialogElement;
+      if (modal) {
+        modal.close();
+      }
+    } catch (error) {
+      console.error("Error creating invoice:", error);
+    }
+  };
+
   return (
-     <Wrapper>
-      salam
-     </Wrapper>
-  )
-} 
+    <Wrapper>
+      <div className="flex flex-col space-y-4">
+        <h1 className="text-lg font-bold">My Factures</h1>
+
+        <div className=" grid md:grid-cols-3 gap-4">
+          <div
+            className="cursor-pointer border border-accent rounded-xl flex flex-col justify-center items-center p-5"
+            onClick={() =>
+              (
+                document.getElementById("my_modal_3") as HTMLDialogElement
+              ).showModal()
+            }>
+            <div className="font-bold text-accent">Create a Facture</div>
+            <div className="bg-accent-content text-accent  rounded-full p-2 mt-2">
+              <Layers className="h-6 w-6" />
+            </div>
+          </div>
+        </div>
+
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+
+            <h3 className="font-bold text-lg">New Facture</h3>
+
+            <input
+              type="text"
+              placeholder="cant be more than 60"
+              className="input input-bordered w-full my-4"
+              value={invoiceName}
+              onChange={(e) => setInvoiceName(e.target.value)}
+            />
+
+            {!isNameValid && (
+              <p className="mb-4 text-sm">cant be more than 60</p>
+            )}
+
+            <button
+              className="btn btn-accent"
+              disabled={!isNameValid || invoiceName.length === 0}
+              onClick={handleCreateInvoice}>
+              Create
+            </button>
+          </div>
+        </dialog>
+      </div>
+    </Wrapper>
+  );
+}
